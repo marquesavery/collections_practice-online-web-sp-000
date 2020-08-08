8e4402034200e1c03d967f0b3656a3ae0aee691f
Marques Avery <averymarques4@gmail.com>
2020-08-08 02:08:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,5 +43,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| element < "s" if index != 1}
+  array.each_with_index.collect do |element, index|
+    element < "s" if index != 1
 end
